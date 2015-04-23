d8b37c74863c4f3536e1be8f75faf4e2dc7eb49d
Hanwen Cheng <heawen.cheng@gmail.com>
2015-04-23 14:32:43 +0200

    Update server2.go

--- server2.go
+++ server2.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:]) //drops the leading "/"
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
